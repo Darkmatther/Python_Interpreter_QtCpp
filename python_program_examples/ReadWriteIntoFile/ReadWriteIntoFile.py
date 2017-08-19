d38cc82182e4ab7e95ca375c23db4f5988701aaa
Darkmatther <mat.kieffer@wanadoo.fr>
2017-08-19 21:40:06 +0200

    Update ReadWriteIntoFile.py

--- python_program_examples/ReadWriteIntoFile/ReadWriteIntoFile.py
+++ python_program_examples/ReadWriteIntoFile/ReadWriteIntoFile.py
@@ -1,1 +1,8 @@
-
+file = open(“textfile.txt”,”w”) 
+ 
+file.write(“Hello World”) 
+file.write(“This is our new text file”) 
+file.write(“and this is another line.”) 
+file.write(“Why? Because we can.”) 
+ 
+file.close() 
