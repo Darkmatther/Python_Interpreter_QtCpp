f6e21d2fdeb8031707d9ee390355f1a93600f5e6
Darkmatther <mat.kieffer@wanadoo.fr>
2017-08-19 21:17:37 +0200

    Create node.py

---
+++ python_program_examples/AStar_Pathfinding/node.py
@@ -0,0 +1,35 @@
+
+
+__author__ = 'hawker'
+import math
+class Node:
+    xPos = 0 # x position
+    yPos = 0 # y position
+    distance = 0 # total distance already travelled to reach the node
+    priority = 0 # priority = distance + remaining distance estimate
+    def __init__(self, xPos, yPos, distance, priority):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.distance = distance
+        self.priority = priority
+    def __lt__(self, other): # comparison method for priority queue
+        return self.priority < other.priority
+    def updatePriority(self, xDest, yDest):
+        self.priority = self.distance + self.estimate(xDest, yDest) * 10 # A*
+    # give higher priority to going straight instead of diagonally
+    def nextMove(self, directions, d): # d: direction to move
+        if directions == 8 and d % 2 != 0:
+            self.distance += 14
+        else:
+            self.distance += 10
+    # Estimation function for the remaining distance to the goal.
+    def estimate(self, xDest, yDest):
+        xd = xDest - self.xPos
+        yd = yDest - self.yPos
+        # Euclidian Distance
+        d = math.sqrt(xd * xd + yd * yd)
+        # Manhattan distance
+        # d = abs(xd) + abs(yd)
+        # Chebyshev distance
+        # d = max(abs(xd), abs(yd))
+        return(d)
